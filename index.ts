eec58b31ca5f38d58003b7d7556efa5fefb27cff
Maxime Pires <maximepires4@gmail.com>
2023-10-24 14:28:42 +0200

    feat: Guess the word

--- index.ts
+++ index.ts
@@ -1,9 +1,56 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
 async function handler(_req: Request): Promise<Response> {
-  return new Response("Hello World");
+  const guess = await extractGuess(_req);
+  const simil = await similarity(guess, "chien");
+  const response = responseBuilder(guess, simil);
+  return new Response(JSON.stringify(response));
 }
 
 serve(handler);
 
+const extractGuess = async (req: Request) => {
+  const slackPayload = await req.formData();
+  const guess = await slackPayload.get("text")?.toString();
+  if (!guess) {
+    throw Error("Guess is empty or null");
+  }
+  return guess;
+};
 
+const similarity = async (word1: string, word2: string) => {
+  const body = {
+    sim1: word1,
+    sim2: word2,
+    lang: "fr",
+    type: "General Word2Vec",
+  };
+  const similarityResponse = await fetch(
+    "http://nlp.polytechnique.fr/similarityscore",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    }
+  );
+  const similarityResponseJson = await similarityResponse.json();
+  return Number(similarityResponseJson.simscore);
+}
+
+const responseBuilder = (guess: string, similarity: number) => {
+  if (similarity === 1) {
+    const response = {
+      response_type: "in_channel",
+      text: `You found the word!`,
+    };
+    return response;
+  }
+
+  const response = {
+    response_type: "in_channel",
+    text: `Similarity: ${similarity*100}% (${guess})`,
+  };
+  return response;
+}
