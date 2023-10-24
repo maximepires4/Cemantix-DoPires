42c0868f2af8376dc28e33f851fe420f7e8e11e7
Maxime Pires <maximepires4@gmail.com>
2023-10-24 14:38:17 +0200

    feat: correction

--- index.ts
+++ index.ts
@@ -1,10 +1,15 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
 async function handler(_req: Request): Promise<Response> {
-  const guess = await extractGuess(_req);
-  const simil = await similarity(guess, "chien");
-  const response = responseBuilder(guess, simil);
-  return new Response(JSON.stringify(response));
+  try {
+    const guess = await extractGuess(_req);
+    const simil = await similarity(guess, "chien");
+    const response = responseBuilder(guess, simil);
+    return new Response(response);
+
+  } catch (error) {
+    return new Response("An error occured ", error);
+  }
 }
 
 serve(handler);
@@ -41,16 +46,28 @@ const similarity = async (word1: string, word2: string) => {
 
 const responseBuilder = (guess: string, similarity: number) => {
   if (similarity === 1) {
-    const response = {
-      response_type: "in_channel",
-      text: `You found the word!`,
-    };
-    return response;
+    return 'You found the word!';
+  } else if (similarity >= 0.9) {
+    return `${similarity * 100}: You are very close!`;
+  } else if (similarity >= 0.8) {
+    return `${similarity * 100}: You are close!`;
+  } else if (similarity >= 0.7) {
+    return `${similarity * 100}: You are almost there!`;
+  } else if (similarity >= 0.6) {
+    return `${similarity * 100}: You are getting there!`;
+  } else if (similarity >= 0.5) {
+    return `${similarity * 100}: You are on the right track!`;
+  } else if (similarity >= 0.4) {
+    return `${similarity * 100}: You are getting closer!`;
+  } else if (similarity >= 0.3) {
+    return `${similarity * 100}: You are far`;
+  } else if (similarity >= 0.2) {
+    return `${similarity * 100}: You are very far`;
+  } else if (similarity >= 0.1) {
+    return `${similarity * 100}: You are so bad`;
+  } else if (similarity >= 0) {
+    return `${similarity * 100}: You are so bad I want to kill myself`;
   }
 
-  const response = {
-    response_type: "in_channel",
-    text: `Similarity: ${similarity*100}% (${guess})`,
-  };
   return response;
 }
